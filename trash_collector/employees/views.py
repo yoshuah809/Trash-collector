b982c18cf02fd12cd31f15cf50cbaf5f2059f86f
unknown <yoshuah809@gmail.com>
2021-10-25 14:23:52 -0500

    completed the Two steps authentication process

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -2,17 +2,36 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.apps import apps
+from django.core.exceptions import ObjectDoesNotExist
 from .models import Employee
+from datetime import date
 
 # Create your views here.
 
 # TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
 
-
 def index(request):
-    # This line will get the Customer model from the other app, it can now be used to query the db for Customers
-    Customer = apps.get_model('customers.Customer')
-    return render(request, 'employees/index.html')
+    # The following line will get the logged-in user (if there is one) within any view function
+    logged_in_user = request.user
+    try:
+        # This line will return the customer record of the logged-in user if one exists
+        logged_in_employee = Employee.objects.get(user=logged_in_user)
+
+        today = date.today()
+        
+        context = {
+            'logged_in_employee': logged_in_employee,
+            'today': today
+        }
+        return render(request, 'employees/index.html', context)
+    except ObjectDoesNotExist:
+        return HttpResponseRedirect(reverse('employees:create'))
+
+# def index(request):
+#     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
+#     Customer = apps.get_model('customers.Customer')
+
+#     return render(request, 'employees/index.html')
 
 def create(request):
     logged_in_user = request.user
@@ -21,6 +40,6 @@ def create(request):
         zip_from_form = request.POST.get('zip_code')
         new_employee = Employee(name=name_from_form, user=logged_in_user, zip_code=zip_from_form)
         new_employee.save()
-        return HttpResponseRedirect(reverse('employee:index'))
+        return HttpResponseRedirect(reverse('employees:index'))
     else:
         return render(request, 'employees/create.html')
