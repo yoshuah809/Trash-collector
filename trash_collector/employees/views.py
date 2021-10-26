f4373da4e73209ec703173c5024ebce40a570abf
unknown <yoshuah809@gmail.com>
2021-10-26 13:40:31 -0500

    Added Dropdown Menu to Filter by selected day

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -104,4 +104,6 @@ def confirm_pickup(request, customer_id):
         customer_confirm.balance += 20
         customer_confirm.save()
         return render(request, 'employees/index.html', context)
-        #return 
+        
+def filter_by_day(request, day):
+    pass        
